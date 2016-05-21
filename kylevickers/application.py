0400976535af44af9b44e699981462937450662b
Kyle Vickers <kylevickers@gmail.com>
2016-05-20 21:14:06 -0400

    Basic outline of PR templater

--- kylevickers/application.py
+++ kylevickers/application.py
@@ -1,5 +1,13 @@
+# Native imports
+import logging
+
+# 3rd Party imports
 from flask import Flask
 from flask import render_template
+from flask import request
+
+# Logging setup
+log = logging.getLogger('werkzeug')
 
 app = Flask(__name__)
 
@@ -7,9 +15,38 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route("/pull", methods=['POST', 'GET'])
+def pull():
+    # Create the pull request and display the string
+    if request.method == 'POST':
+        formatted_response = create_pull_request(
+            summary=request.form['summary'],
+            ticket=request.form['ticket'],
+        )
+        print('Formatted Response...')
+        print(formatted_response)
+        return render_template('pr.html', output=formatted_response)
+    # Display the PR template
+    if request.method == 'GET':
+        return render_template('pull.html')
+
+
+def create_pull_request(summary, ticket):
+    pull_request_format = """
+    # Summary
+    {summary}
+
+    # Ticket
+    {ticket}
+    """
+
+    return pull_request_format.format(summary=summary, ticket=ticket)
+
+# Start the debug server
 if __name__ == "__main__":
     # Set debug mode on
     app.debug = True
+    log.debug('new message')
 
     # Start the app
     app.run()
